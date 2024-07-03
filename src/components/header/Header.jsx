a1817607aad9ed83ca429307f495ae437dbe34d4
Elias <101352052+eliascending@users.noreply.github.com>
2024-07-03 15:24:22 +0200

    sérieux

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <nav>
+
+            </nav>
+        </header>
+    );
+}
+export default Header;
