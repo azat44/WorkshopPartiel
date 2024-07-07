0e1fe8f163546cc05afa91f9af80c8d866930580
Elias <101352052+eliascending@users.noreply.github.com>
2024-07-07 17:24:02 +0200

    page favoris crée

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,23 @@ import Footer from "./components/footer/Footer";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProductPage from "./pages/ProductPage";
+import Favs from "./pages/Favs";
 
 function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/produits" element={<ProductPage/>}></Route>
-        <Route path="/notre-univers" element={<Univers/>}></Route>
-        <Route path="/produit" element={<Produit/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
-      </Routes>
-    <Footer/>
-    </BrowserRouter>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/produits" element={<ProductPage/>}></Route>
+          <Route path="/notre-univers" element={<Univers/>}></Route>
+          <Route path="/produit" element={<Produit/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
+          <Route path="/fav" element={<Favs/>}></Route>
+        </Routes>
+      <Footer/>
+      </BrowserRouter>
   )
 }
 
---
+++ src/pages/Favs.jsx
@@ -0,0 +1,10 @@
+import styles from './Favs.module.css'
+
+const Favs = () => {
+    return (
+        <section className={styles.section}>
+            <h1>Favoris</h1>
+        </section>
+    );
+}
+export default Favs;
